4bd0e559034e63430bc05aa507bfd298c1ffbcf4
Sakshi Kulkarni <sakshi29kulkarni@gmail.com>
2024-07-05 14:29:49 +0530

    Full website UI Design done

--- client/src/component/About.js
+++ client/src/component/About.js
@@ -1,12 +1,87 @@
-import React from 'react'
+import React from "react";
+import aboutimg from "../images/Japan.jpg";
 
 const About = () => {
   return (
-    <div>
-      <p>WELCOME</p>
-      <p>We are the About us developers</p>
-    </div>
-  )
-}
+    <>
+      <div className="container">
+        <div className="first-col">
+          <div className="about-title">
+            <h5>The best travel company</h5>
+            <h2 className="form-title">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard
+            </h2>
+          </div>
+          <div className="about-image">
+            <img src={aboutimg}></img>
+          </div>
+        </div>
+        <br /> <br />
+        <div className="second-col">
+          <div className="about-text">
+            <p>
+              Contrary to popular belief, Lorem Ipsum is not simply random text.
+              It has roots in a piece of classical Latin literature from 45 BC,
+              making it over 2000 years old. Richard McClintock, a Latin
+              professor at Hampden-Sydney College in Virginia, looked up one of
+              the more obscure Latin words, consectetur, from a Lorem Ipsum
+              passage, and going through the cites of the word in classical
+              literature, discovered the undoubtable source. Lorem Ipsum comes
+              from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et
+              Malorum" (The Extremes of Good and Evil) by Cicero, written in 45
+              BC. This book is a treatise on the theory of ethics, very popular
+              during the Renaissance. The first line of Lorem Ipsum, "Lorem
+              ipsum dolor sit amet..", comes from a line in section 1.10.32. The
+              standard chunk of Lorem Ipsum used since the 1500s is reproduced
+              below for those interested. Sections 1.10.32 and 1.10.33 from "de
+              Finibus Bonorum et Malorum" by Cicero are also reproduced in their
+              exact original form, accompanied by English versions from the 1914
+              translation by H. Rackham.
+            </p>
+          </div>
+          <div className="about-facts">
+            <div className="fact-row1">
+              <div className="fact1">
+                <h2 className="fact-title">
+                  3.5
+                </h2>
+                <h5 className="fact-body">
+                  Overall Reviews
+                </h5>
+              </div>
+              <div className="fact2">
+              <h2 className="fact-title">
+                  23
+                </h2>
+                <h5 className="fact-body">
+                  Satisfied Customers
+                </h5>
+              </div>
+            </div>
+            <div className="fact-row2">
+            <div className="fact3">
+                <h2 className="fact-title">
+                 830+
+                </h2>
+                <h5 className="fact-body">
+                  Sucessful trips
+                </h5>
+              </div>
+              <div className="fact4">
+              <h2 className="fact-title">
+                  100K
+                </h2>
+                <h5 className="fact-body">
+                  Revenue
+                </h5>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default About
+export default About;
--- client/src/component/Home.js
+++ client/src/component/Home.js
@@ -1,12 +1,112 @@
-import React from 'react'
+import React from "react";
+import { NavLink } from "react-router-dom";
+import goa from "../images/goa.jpg";
+import greese from "../images/Greece.jpg";
+import iceland from "../images/Iceland.jpg";
+import italy from "../images/italy.jpg";
+import varanasi from "../images/Varanasi.jpg";
+import japan from "../images/Japan.jpg";
+import paris from "../images/paris.jpg";
+import rome from "../images/rome.jpg";
+import tokyo from "../images/tokyo.jpg";
+import turkey from "../images/Turkey.jpg";
 
 const Home = () => {
   return (
-    <div>
-      <p className='pt-5'>WELCOME</p>
-      <p>We are the mern developers</p>
-    </div>
-  )
-}
+    <>
+      <header>
+        <section className="hero">
+          <div className="hero-inner">
+            <h1>WandurLust</h1>
+            <NavLink to="" className="hero-btn">
+              Explore Places
+            </NavLink>
+          </div>
+        </section>
+      </header>
 
-export default Home
+      {/* destination section */}
+      <br />
+      <h2 className="form-title"> Explore Destinations </h2>
+      <div class="imageGrid" id="imageGrid">
+        <div class="tile imagegr1">
+          <div class="textWrapper">
+            <h2>Greese</h2>
+            <div class="content">
+            </div>
+          </div>
+        </div>
+        <div class="tile imagegr2">
+          <div class="textWrapper">
+            <h2>Paris</h2>
+            <div class="content">
+            </div>
+          </div>
+        </div>
+        <div class="tile imagegr3">
+          <div class="textWrapper">
+            <h2>Rome</h2>
+            <div class="content">
+            </div>
+          </div>
+        </div>
+        <div class="tile imagegr4">
+          <div class="textWrapper">
+            <h2>Turkey</h2>
+            <div class="content">
+            </div>
+          </div>
+        </div>
+        <div class="tile imagegr5">
+          <div class="textWrapper">
+            <h2>Tokyo</h2>
+            <div class="content">
+              </div>
+          </div>
+        </div>
+        <div class="tile imagegr6">
+          <div class="textWrapper">
+            <h2>Varanasi</h2>
+            <div class="content"></div>
+          </div>
+        </div>
+      </div>
+
+      {/* footer */}
+      <footer class="site-footer">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-8 col-sm-6 col-xs-12">
+              <p class="copyright-text">
+                Copyright &copy; 2024 All Rights Reserved by <br/>
+                <a href="#">Sakshi Kulkarni</a>.
+              </p>
+            </div>
+
+            <div class="col-md-4 col-sm-6 col-xs-12">
+              <ul class="social-icons">
+                <li className="icon-single">
+                <i class="zmdi zmdi-linkedin-box"></i>
+                </li>
+                <li className="icon-single">
+                <i class="zmdi zmdi-facebook-box"></i>
+                </li>
+                <li className="icon-single">
+                <i class="zmdi zmdi-github"></i>
+                </li>
+                <li className="icon-single">
+                <i class="zmdi zmdi-pinterest-box"></i>
+                </li>
+                <li className="icon-single">
+                <i class="zmdi zmdi-twitter-box"></i>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+};
+
+export default Home;
